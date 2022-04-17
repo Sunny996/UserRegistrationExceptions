6d64b489ce051dd94febca39d06ff960f09ec767
Sunny996 <satlaakash@gmail.com>
2022-04-17 13:07:06 +0530

    ADDED: A regex for first name

---
+++ src/main/java/com/JUnit/Main.java
@@ -0,0 +1,10 @@
+package com.JUnit;
+
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        System.out.println(Pattern.matches(userRegistration.firstName, "Akash"));
+    }
+}
---
+++ src/main/java/com/JUnit/UserRegistration.java
@@ -0,0 +1,6 @@
+package com.JUnit;
+
+public class UserRegistration {
+    public String firstName = "^[A-Z][a-z]{2,}$";
+
+}
