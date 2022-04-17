244fc02cdf6b631dc30a5deb013290c01b096498
Sunny996 <satlaakash@gmail.com>
2022-04-17 13:16:06 +0530

    ADDED: A regex to match phone number requirements

--- src/main/java/com/JUnit/UserRegistration.java
+++ src/main/java/com/JUnit/UserRegistration.java
@@ -4,5 +4,6 @@ public class UserRegistration {
     public String firstName = "^[A-Z][a-z]{2,}$";
     public String lastName = "^[A-Z][a-z]{2,}$";
     public String email = "^[a-z0-9_+-]+((\\.)[a-z0-9]+)?(\\@)[a-z0-9]+(\\.)[a-z]{2,4}((\\.)[a-z]{2,4})?$";
+    public String phoneNumber = "^[1-9][0-9]{0,2} [1-9][0-9]{9}$";
 
 }
