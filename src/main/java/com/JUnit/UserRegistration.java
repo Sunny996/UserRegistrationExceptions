7f3201575e389cd9dccb455049ca0eedfcaa7b9e
Sunny996 <satlaakash@gmail.com>
2022-04-17 13:33:35 +0530

    ADDED: A regex to match password of min 8 characters with caps & digits & special characters

--- src/main/java/com/JUnit/UserRegistration.java
+++ src/main/java/com/JUnit/UserRegistration.java
@@ -5,6 +5,6 @@ public class UserRegistration {
     public String lastName = "^[A-Z][a-z]{2,}$";
     public String email = "^[a-z0-9_+-]+((\\.)[a-z0-9]+)?(\\@)[a-z0-9]+(\\.)[a-z]{2,4}((\\.)[a-z]{2,4})?$";
     public String phoneNumber = "^[1-9][0-9]{0,2} [1-9][0-9]{9}$";
-    public String password = "^(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9]{8,}$";
+    public String password = "^(?=.*[A-Z])(?=.*\\d)(?=[a-zA-Z0-9]*[@$#%^&!?][a-zA-Z0-9]*$)[a-zA-Z0-9!@#$^&*]{8,}$";
 
 }
