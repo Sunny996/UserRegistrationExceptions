e089b12fb9a89f2ab202ac3f4d0c7bb5a546af17
Sunny996 <satlaakash@gmail.com>
2022-04-17 13:12:57 +0530

    ADDED: A regex for email address requirements

--- src/main/java/com/JUnit/UserRegistration.java
+++ src/main/java/com/JUnit/UserRegistration.java
@@ -3,5 +3,6 @@ package com.JUnit;
 public class UserRegistration {
     public String firstName = "^[A-Z][a-z]{2,}$";
     public String lastName = "^[A-Z][a-z]{2,}$";
+    public String email = "^[a-z0-9_+-]+((\\.)[a-z0-9]+)?(\\@)[a-z0-9]+(\\.)[a-z]{2,4}((\\.)[a-z]{2,4})?$";
 
 }
